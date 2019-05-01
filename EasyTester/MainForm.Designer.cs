0876a6db5621fa80c774ef5d4c81a206d1c52378
plyskay@mail.ru <yuriyply@gmail.com>
2019-05-01 19:44:21 +0300

    Add project files.

---
+++ EasyTester/MainForm.Designer.cs
@@ -0,0 +1,119 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Plyskay
+ * Date: 5/23/2018
+ * Time: 1:59 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+namespace EasyTester
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Timer timer1;
+		private System.Windows.Forms.ListBox listBox1;
+		private System.Windows.Forms.ProgressBar progressBar1;
+		private System.Windows.Forms.Label label2;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(217, 100);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(180, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.TextBox1TextChanged);
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TextBox1KeyPress);
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(118, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(425, 34);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Введіть серійний номер і натисніть ENTER";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.Timer1Tick);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(91, 156);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(361, 225);
+            this.listBox1.TabIndex = 3;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.BackColor = System.Drawing.Color.White;
+            this.progressBar1.Location = new System.Drawing.Point(91, 413);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(20, 16);
+            this.progressBar1.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(12, 413);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(73, 16);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Easy_Tester";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(621, 464);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "EasyTester";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+		}
+	}
+}
---
+++ EasyTester/MainForm.cs
@@ -0,0 +1,270 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+using System.IO.Ports;
+using System.IO;
+using System.Threading;
+using System.Data;
+
+namespace EasyTester
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{	
+		public string SerNum;
+		SerialPort _serpotr1 = new SerialPort("COM1");	
+		string _USB_check;
+		string _Plus_B;
+		string _Minus_B;
+		string _Phone_B;
+		string _Play_B;
+		string _Conn_B;
+		string _start;
+		string _end;
+		string [] cultureName = {"de-DE"};
+		DateTime start;
+		DateTime end;
+		TimeSpan ts;
+	
+		public MainForm()
+		{
+			InitializeComponent();
+			progressBar1.Value = 100;
+	        progressBar1.BackColor = Color.Red;
+	       
+		}
+		void TextBox1TextChanged(object sender, EventArgs e)
+		{
+			
+		}
+		void TextBox1KeyPress(object sender, KeyPressEventArgs e)
+		{
+		
+			if (e.KeyChar == (char)Keys.Return) {
+				//MessageBox.Show(textBox1.Text);
+				SerNum = textBox1.Text;
+				start = DateTime.Now;
+				_start = start.ToString(cultureName[0]);
+				
+				//listBox1.Items.ToString() = textBox1.Text;
+				if (!CheckSerNumb(SerNum)) {
+					MessageBox.Show("Неправильний серійний номер");
+					textBox1.Text = "";
+					SerNum = String.Empty;
+					return;
+				}
+			//add d0-while//
+		
+		CheckTester();
+			
+		if(USB_check())
+		{
+			
+			Fail(1); _serpotr1.Close(); end = DateTime.Now; _end = end.ToString(cultureName[0]);
+			return; 
+		}
+		
+		if(Plus_B()){
+			Fail(2); _serpotr1.Close(); end = DateTime.Now; _end = end.ToString(cultureName[0]);
+			return;}
+		if(Minus_B()){
+			Fail(3); _serpotr1.Close(); end = DateTime.Now; _end = end.ToString(cultureName[0]);
+		    return;}
+		if(Phone_B()){
+			Fail(4); _serpotr1.Close(); end = DateTime.Now; _end = end.ToString(cultureName[0]);
+			return;}
+		if(Play_B()){
+			Fail(5); _serpotr1.Close(); end = DateTime.Now; _end = end.ToString(cultureName[0]);
+			return;}
+		if(Conn_B()){
+			Fail(6); _serpotr1.Close(); end = DateTime.Now; _end = end.ToString(cultureName[0]);
+			return;}
+		   MessageBox.Show("PASS");
+		}
+	}
+		
+		
+		
+		
+		
+		bool CheckSerNumb(string SerNumb)
+		{
+			int x = SerNumb.Length;
+			if((x == 10) & (SerNumb.Contains("SU") || SerNumb.Contains("AX"))){
+			   	return true; }
+			   	return false;
+		}
+//		void Button1Click(object sender, EventArgs e)
+//		{
+//			MainForm form = new MainForm();
+//			//form.TextBox1KeyPress += 
+//	    }
+		void CheckTester()
+		{
+			//byte [] buff;
+			
+			listBox1.Items.Add("Checking connection to the tester");
+			try{
+				_serpotr1.BaudRate = 9600;
+				_serpotr1.Parity = Parity.None;
+				_serpotr1.StopBits = StopBits.One;
+				_serpotr1.Handshake = Handshake.None;
+				_serpotr1.Open();
+				_serpotr1.RtsEnable = true;
+				_serpotr1.DtrEnable = true;
+				//_serpotr1.Write("1");
+				//_serpotr1.BytesToWrite(1);
+				
+				
+			   }catch(IOException ex)
+			{
+				MessageBox.Show(ex.ToString());
+			}
+//			_serpotr1.Write(new byte[] {1}, 0,1 );
+//				Thread.Sleep(1000);
+//			 arduino = _serpotr1.ReadLine();
+//			 if(arduino.Contains("OK"))
+//				
+//				listBox1.Items.Add("Connection is OK");
+//				
+//			else _serpotr1.Close();
+//					
+//			return true;
+		
+		}
+		bool ReadComPort()
+		{   listBox1.Items.Add ("Checking connection");
+			string arduino= string.Empty;
+			 double e = 0.0;
+			 DateTime u = DateTime.Now;
+			do
+			{
+				DateTime u2 = DateTime.Now;
+				ts =u2-u; e = ts.TotalSeconds;
+			_serpotr1.Write("1");
+			Thread.Sleep(2000);
+			arduino = _serpotr1.ReadLine();
+			
+			}while((e >= 15.0) || _serpotr1.ReadLine() == "OK");
+		
+			if(arduino.Contains("OK"))
+				return false;
+			return true;
+		}
+		
+		void Timer1Tick(object sender, EventArgs e)
+		{
+	
+		}
+		bool USB_check()
+		{   listBox1.Items.Add ("Testing USB");
+			 double r = 0.0;
+			 DateTime u = DateTime.Now;
+			do{
+				DateTime u2 = DateTime.Now;
+				ts =u2-u; r = ts.TotalSeconds;
+			if(_serpotr1.ReadLine() == "USB"){
+					_USB_check = "PASS";  listBox1.Items.Add (" USB - OK");   return false;}
+				}while( (r >= 15.0) || _serpotr1.ReadLine() == "USB" || _serpotr1.ReadLine() == "FAIL");
+			  listBox1.Items.Add ("USB - Fail");
+				return true;
+	    }
+		
+	//Method Fail//
+	   string Fail(int x)
+	   {
+	   	switch(x)
+	   	{
+				case 1:
+					 _USB_check = "FAIL";
+					 break;
+				case 2:
+					 _Plus_B = "FAIL";
+					 break;
+				case 3:
+					 _Minus_B = "FAIL";
+					break;
+				case 4: 
+					 _Phone_B = "FAIL";
+					 break;
+				case 5:
+					 _Play_B = "FAIL";
+					 break;
+				case 6: 
+					 _Conn_B = "FAIL";
+					 break;
+		}
+			return "PASS";
+		}
+	   // Buttons/// 
+	
+	    bool Plus_B()
+	    {      listBox1.Items.Add ("Testing Plus_B");   
+	    	double r1 =0.0f;
+	        DateTime p1 = DateTime.Now;
+	    	do{
+	        	DateTime p2 = DateTime.Now;
+				ts =p2-p1; r1 = ts.TotalSeconds;
+	   	  if(_serpotr1.ReadLine() == "Plus"){
+	    			_Plus_B = "PASS"; listBox1.Items.Add ("Plus_B - OK"); return false;}
+	    	}while(r1 >= 15 || _serpotr1.ReadLine() == "Plus" || _serpotr1.ReadLine() == "FAIL");
+	   	     listBox1.Items.Add ("Plus_B - Fail"); return true;
+	    }
+		bool Minus_B()
+	    {   listBox1.Items.Add ("Testing Minus_B");
+			double r2 = 0.0f;
+			 DateTime m1 = DateTime.Now;
+			do{
+			 	DateTime m2 = DateTime.Now;
+				ts =m2-m1; r2 = ts.TotalSeconds;
+		 	if(_serpotr1.ReadLine() == "Minus"){
+	   		_Minus_B = "PASS";listBox1.Items.Add ("Minus_B - OK"); return false;}
+			}while(r2 >= 15 || _serpotr1.ReadLine() == "Minus" || _serpotr1.ReadLine() == "FAIL");
+			 listBox1.Items.Add ("Minus_B-Fail");
+	   	    return true;
+	    }
+		bool Phone_B()
+	    {    listBox1.Items.Add ("Testing Phone_B");
+			 double r3 = 0.0f;
+			 DateTime p1 = DateTime.Now;
+			do{
+			 	DateTime p2 = DateTime.Now;
+				ts =p2-p1; r3 = ts.TotalSeconds;
+		  	if(_serpotr1.ReadLine() == "Phone"){
+	   		_Phone_B = "PASS";  listBox1.Items.Add ("Phone_B - OK"); return false;}
+			}while(r3 >= 15 || _serpotr1.ReadLine() == "Phone" || _serpotr1.ReadLine() == "FAIL");
+			  listBox1.Items.Add ("Phone_B-Fail");
+	   	    return true;
+	    }
+		bool Play_B()
+	    {    listBox1.Items.Add ("Testing Play_B");
+			 double r4 = 0.0f;
+			 DateTime p2 = DateTime.Now;
+			 do{
+			 	DateTime p3 = DateTime.Now;
+				ts =p3-p2; r4 = ts.TotalSeconds;
+		 	if(_serpotr1.ReadLine() == "Play"){
+	   		_Play_B = "PASS";  listBox1.Items.Add ("Play_B - OK"); return false;}
+			 }while(r4 >= 15 || _serpotr1.ReadLine() == "Play" || _serpotr1.ReadLine() == "FAIL");
+			  listBox1.Items.Add ("Play_B-Fail");
+	   	    return true;
+	    }
+		bool Conn_B()
+	    {    listBox1.Items.Add ("Testing Conn_B");
+			double r5 = 0.0f;
+			 DateTime c1 = DateTime.Now;
+			 do{
+			 	DateTime c2 = DateTime.Now;
+				ts =c2-c1; r5 = ts.TotalSeconds;
+		  	if(_serpotr1.ReadLine() == "Conn"){
+	   		_Conn_B = "PASS";  listBox1.Items.Add ("Conn_B - OK"); return false;}
+			 }while(r5 >= 15 || _serpotr1.ReadLine() == "Conn" || _serpotr1.ReadLine() == "FAIL" );
+			  listBox1.Items.Add ("Conn_B-Fail");
+	   	    return true;
+	    }
+		 
+     }
+}
